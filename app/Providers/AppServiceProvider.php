eea65dfa26e2acfa236db30c8a23a14df17b39a6
heolazz <heolazz.gmail.com>
2025-05-25 21:13:55 +0700

    Add built assets

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -33,5 +34,10 @@ class AppServiceProvider extends ServiceProvider
 
         $view->with('randomTip', $randomTip);
     });
+
+     if (app()->environment('production')) {
+        URL::forceScheme('https');
+    }
 }
 }
+
