8ca9e4a2e49bfac7dc83276f096a2f30b7291ff3
heolazz <heolazz.gmail.com>
2025-06-06 15:13:12 +0700

    fix error css

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Blade;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -34,6 +35,9 @@ class AppServiceProvider extends ServiceProvider
 
         $view->with('randomTip', $randomTip);
     });
+
+    Blade::componentNamespace('App\\View\\Components', 'components');
+    // ...
 
      if (app()->environment('production')) {
         URL::forceScheme('https');
